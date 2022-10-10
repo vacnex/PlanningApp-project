3228555f7c03a1abc6265714803207d2f550ffd3
VAC <37587399+vacnex@users.noreply.github.com>
2022-10-10 18:16:25 +0700

    lint code

--- src/server.js
+++ src/server.js
@@ -21,34 +21,56 @@ server.use(middlewares);
 // server.use(bodyParser.urlencoded({ extended: true }));
 // server.use(bodyParser.json());
 
-// Create a token from a payload 
+/** 
+* Create a token from a payload.
+* @param {any} payload - Data need encryption.
+* @return {string} Encryption token.
+*/
 function createToken(payload) {
   return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
-// Verify the token 
+/** 
+* Verify the token.
+* @param {string} token - token need verify.
+* @return {boolean} True if token is valid and false for invalid token.
+*/
 function verifyToken(token) {
   return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err);
 }
 
-
-function isAuthenticated({ username, password }) {
+/** 
+* Check user exist in database.
+* @param {Object} account - The account model.
+* @param {string} account.username - The username.
+* @param {string} account.password - The password.
+* @return {boolean} True if user exist in database and false if not found user.
+*/
+function IsUserExist({ username, password }) {
   return data.Users.findIndex(user => user.username === username && user.password === password) !== -1;
 }
-function getUserInfo({ username, password }) {
+/** 
+* get user role and id.
+* @param {Object} account - The account model.
+* @param {string} account.username - The username.
+* @param {string} account.password - The password.
+* @return  return object with role and id information
+*/
+function GetUserInfo({ username, password }) {
   const user = data.Users.find(user => user.username === username && user.password === password);
   return { id: user.id, role: user.role };
 }
 
+
 server.post('/api/login', (req, res) => {
   const { username, password } = req.query;
-  if (isAuthenticated({ username, password }) === false) {
+  if (IsUserExist({ username, password }) === false) {
     const status = 401;
     const message = 'Sai tên đăng nhập hoặc mật khẩu';
     res.status(status).json({ status, message });
     return;
   }
-  const { id, role } = getUserInfo({ username, password });
+  const { id, role } = GetUserInfo({ username, password });
   const access_token = createToken({ id, role });
   res.status(200).json({ access_token });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +15,8 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    }),],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
